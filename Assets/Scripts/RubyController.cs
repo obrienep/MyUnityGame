32410fd896b1219dd3dbd860e01923740a58fdbc
DeepFriedTwinkie123 <eobrien185@gmail.com>
2022-02-06 19:19:58 -0500

    Added 2D Mega Pack

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float smoothing = 5f;
     void Start()
     {
         
