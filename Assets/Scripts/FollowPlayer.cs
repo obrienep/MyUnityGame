d5cdd49cdc0107ef0868fe436d4fe0c1aba8a898
DeepFriedTwinkie123 <eobrien185@gmail.com>
2022-02-06 19:06:08 -0500

    Added Ruby Scene

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset = new Vector3(0, 0, -10);
+    private Vector3 offset = new Vector3(0, 0, -5);
     // Start is called before the first frame update
     void Start()
     {
